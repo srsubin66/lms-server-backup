88206b6fe28e93471ab0456951872aede571db08
Server <subins@karunya.edu.in>
2025-01-29 17:12:21 +0000

    Initial commit with folder contents

---
+++ apps/lms/lms/lms/config/doc.py
@@ -0,0 +1,8 @@
+def get_context(context):
+    context.brand_html = "LMS"
+    context.pages = [
+        {
+            "page_name": "lms_leaderboard",
+            "title": "LMS Leaderboard"
+        }
+    ]
