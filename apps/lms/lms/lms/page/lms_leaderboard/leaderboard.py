81048bc1669d0d58e2f186d06b3ad90e62655ff8
Subin <subins@karunya.edu.in>
2025-01-30 00:16:47 +0530

    leaderboard5

--- apps/lms/lms/lms/page/lms_leaderboard/leaderboard.py
+++ apps/lms/lms/lms/page/lms_leaderboard/leaderboard.py
@@ -1,23 +1,19 @@
+# Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and Contributors
+# License: MIT. See LICENSE
 import frappe
+def has_permission(doc, ptype, user):
+    # Allow access to everyone who is logged in
+    return frappe.session.user != 'Guest'
 
-def has_permission(doc=None, user=None, doctype=None):
-    """
-    Allow all users to access the leaderboard regardless of the doctype
-    """
-    return True
 
-def has_website_permission(doc=None, user=None, doctype=None):
-    """
-    Allow all website users to access the leaderboard regardless of the doctype
-    """
-    return True
-
+def has_website_permission(doc, ptype, user):
+    return True 
 
 @frappe.whitelist()
 def get_leaderboard_config():
-    leaderboard_config = frappe._dict()
-    leaderboard_hooks = frappe.get_hooks("leaderboards")
-    for hook in leaderboard_hooks:
-        leaderboard_config.update(frappe.get_attr(hook)())
-        
-    return leaderboard_config
+	leaderboard_config = frappe._dict()
+	leaderboard_hooks = frappe.get_hooks("leaderboards")
+	for hook in leaderboard_hooks:
+		leaderboard_config.update(frappe.get_attr(hook)())
+
+	return leaderboard_config
