b66b209c164d858780ad0f0798283be7c64f59b1
URK21CS1067 <93753029+URK21CS1067@users.noreply.github.com>
2025-02-03 18:44:43 +0530

    update progress2

--- apps/lms/lms/lms/page/lms_leaderboard/leaderboard.py
+++ apps/lms/lms/lms/page/lms_leaderboard/leaderboard.py
@@ -1,13 +1,23 @@
 # Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and Contributors
 # License: MIT. See LICENSE
 import frappe
+
 def has_permission(doc, ptype, user):
     # Allow access to everyone who is logged in
-    return frappe.session.user != 'Guest'
-
+    if frappe.session.user == 'Guest':
+        return False
+        
+    # Check if user is either a system user or has an LMS enrollment
+    return (frappe.session.user == 'Administrator' 
+            or frappe.db.exists('User', {'name': frappe.session.user, 'user_type': 'System User'})
+            or frappe.db.exists('LMS Enrollment', {'user': frappe.session.user}))
 
 def has_website_permission(doc, ptype, user):
-    return True 
+    # Allow access to logged-in users with LMS enrollment
+    if frappe.session.user == 'Guest':
+        return False
+    
+    return frappe.db.exists('LMS Enrollment', {'user': frappe.session.user})
 
 @frappe.whitelist()
 def get_leaderboard_config():
